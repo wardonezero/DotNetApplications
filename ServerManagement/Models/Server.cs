07bfb0fb7e039e2b13ce0f43064fd782b36e1dc8
Eduard Snow <eduard.wintersnow@outlook.com>
2024-11-13 16:02:07 +0400

    Implicit razor expression

---
+++ ServerManagement/Models/Server.cs
@@ -0,0 +1,16 @@
+﻿namespace ServerManagement.Models;
+
+public class Server
+{
+    public int ServerId { get; set; }
+    public bool IsOnline { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string City { get; set; } = string.Empty;
+
+    public Server()
+    {
+        Random random = new Random();
+        int randomOnOff = random.Next(0, 2);
+        IsOnline = randomOnOff == 1 ? true : false;
+    }
+}
