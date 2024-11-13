0361fd053e60f1adcefb3a82be694431a94eb1db
Eduard Snow <eduard.wintersnow@outlook.com>
2024-11-13 18:32:33 +0400

    Create a routable component
    Route parameters and route constraints

--- ServerManagement/Models/CitiesRepository.cs
+++ ServerManagement/Models/CitiesRepository.cs
@@ -2,6 +2,6 @@
 
 public static class CitiesRepository
 {
-    private static List<string> cities = new() { "Chicago", "Rockford", "Springfield", "Joliet", "Peoria" };
+    private static List<string> cities = ["Chicago", "Rockford", "Springfield", "Joliet", "Peoria"];
     public static List<string> GetCities() => cities;
 }
