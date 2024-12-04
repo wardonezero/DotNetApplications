56ca7bdeae9d2003c0791ce66fe16d7ab011ea91
Eduard Snow <eduard.wintersnow@outlook.com>
2024-12-04 14:21:30 +0400

    hub method

--- SignalRMVC/Hubs/UserCount.cs
+++ SignalRMVC/Hubs/UserCount.cs
@@ -3,5 +3,11 @@
 namespace SignalRMVC.Hubs;
 public class UserCount : Hub
 {
+    public static byte TotalViewers { get; private set; }
 
+    public async Task NewViewer()
+    {
+        TotalViewers++;
+        await Clients.All.SendAsync("UpdateTotalViewers", TotalViewers);
+    }
 }
