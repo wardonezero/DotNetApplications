40f40142a61f20987dda2af39288c28d179b28ec
Eduard Snow <eduard.wintersnow@outlook.com>
2024-12-07 10:17:36 +0400

    Static details

--- SignalRMVC/Controllers/HomeController.cs
+++ SignalRMVC/Controllers/HomeController.cs
@@ -4,29 +4,37 @@ using SignalRMVC.Models;
 
 namespace SignalRMVC.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController(ILogger<HomeController> logger) : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly ILogger<HomeController> _logger = logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public IActionResult DeathlyHallows(string type)
         {
-            _logger = logger;
+            try
+            {
+                var deathlyHallowType = Enum.Parse<DeathlyHallowType>(type);
+                if (StaticDetails.DealthyHallowRace.ContainsKey(deathlyHallowType))
+                {
+                    StaticDetails.DealthyHallowRace[deathlyHallowType]++;
+                }
+                else
+                {
+                    return BadRequest("Hallow type not found in the race dictionary.");
+                }
+            }
+            catch (ArgumentException)
+            {
+                return View("Index");
+            }
+            return Accepted();
+
         }
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+        public IActionResult Index() => View();
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+        public IActionResult Privacy() => View();
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+        public IActionResult Error() => View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 }
---
+++ SignalRMVC/StaticDetails.cs
@@ -0,0 +1,21 @@
+﻿namespace SignalRMVC;
+
+public static class StaticDetails
+{
+    public static Dictionary<DeathlyHallowType, int> DealthyHallowRace = new(3);
+
+    static StaticDetails()
+    {
+        DealthyHallowRace.Add(DeathlyHallowType.Wand, 0);
+        DealthyHallowRace.Add(DeathlyHallowType.Stone, 0);
+        DealthyHallowRace.Add(DeathlyHallowType.Cloak, 0);
+    }
+
+}
+
+public enum DeathlyHallowType
+{
+    Wand,
+    Stone,
+    Cloak
+}
